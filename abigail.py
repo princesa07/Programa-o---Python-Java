c34862884a596f674837e42585ef1a695555c056
princesa07 <abigailprincesa58@gmail.com>
2025-03-18 09:20:09 -0300

    Atividade 1

---
+++ abigail.py
@@ -0,0 +1,9 @@
+nome = input("Qual o nome do personagem principal? ")
+idade = ("Qual a idade do seu personagem? ")
+poder = input("Qual o poder do seu personagem principal? ")
+fraqueza = input("Qual a sua fraqueza? ")
+vilao = input("Qual o nome do vilão? ")
+climax = input("Qual o climax da sua história? ")
+local = input("Onde se passa o climax? ")
+
+print(f"")
