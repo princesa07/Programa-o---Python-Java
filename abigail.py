a37450a17d80a1f572e639fe8fec59d019140952
princesa07 <abigailprincesa58@gmail.com>
2025-03-19 13:39:16 -0300

    Atividade finalizada

--- abigail.py
+++ abigail.py
@@ -1,9 +1,9 @@
 nome = input("Qual o nome do personagem principal? ")
-idade = ("Qual a idade do seu personagem? ")
+idade = int(input("Qual a idade do seu personagem? "))
 poder = input("Qual o poder do seu personagem principal? ")
-fraqueza = input("Qual a sua fraqueza? ")
+fraqueza = input("Qual será a sua fraqueza? ")
 vilao = input("Qual o nome do vilão? ")
 climax = input("Qual o climax da sua história? ")
 local = input("Onde se passa o climax? ")
 
-print(f"")
+print(f"O guerreiro(a) {nome}, de {idade} anos com o poder de {poder} enfrenta {vilao} em uma acirrada batalha em {local} após {climax}, desesperado(a) a vencer, {vilao} utiliza a maior fraqueza do nosso guerreiro(a), que poucos sabem: {fraqueza}.")
