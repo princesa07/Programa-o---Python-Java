bbd716c93e1e633978d5a4c7dfd961b8872319cf
Miguel-Mendes11 <rodriguesmiguelmendes@gmail.com>
2025-03-28 16:34:10 -0300

    Atividade finalizada

---
+++ At - História interativa/codigo.py
@@ -0,0 +1,9 @@
+nome = input("Qual o nome do personagem principal? ")
+idade = int(input("Qual a idade do seu personagem? "))
+poder = input("Qual o poder do seu personagem principal? ")
+fraqueza = input("Qual será a sua fraqueza? ")
+vilao = input("Qual o nome do vilão? ")
+climax = input("Qual o climax da sua história? ")
+local = input("Onde se passa o climax? ")
+
+print(f"O guerreiro(a) {nome}, de {idade} anos com o poder de {poder} enfrenta {vilao} em uma acirrada batalha em {local} após {climax}, desesperado(a) a vencer, {vilao} utiliza a maior fraqueza do nosso guerreiro(a), que poucos sabem: {fraqueza}.")
